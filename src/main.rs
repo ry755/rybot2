0aeee33b848770b81f393d7311584e0e2a006151
ry755 <ryanst755@gmail.com>
2021-09-18 04:21:08 -0700

    Remove Z80 + TMS9918A emulators and other unused code
    This bumps the version to 0.2.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{convert::TryInto, env, num::NonZeroU16, sync::Arc};
-//use std::process::Command;
+use error_chain::error_chain;
+use std::{env, sync::Arc};
 use serenity::{
     async_trait,
     client::Context,
@@ -16,86 +16,14 @@ use serenity::{
     utils::{content_safe, ContentSafeOptions},
     prelude::*,
 };
-
 use songbird::SerenityInit;
-
-use tms9918a_emu::TMS9918A;
+use tempfile::Builder;
 
 struct ShardManagerContainer;
 
 impl TypeMapKey for ShardManagerContainer {
     type Value = Arc<Mutex<ShardManager>>;
 }
-
-use z80emu::*;
-extern crate hex;
-type TsClock = host::TsCounter<i32>;
-
-// Z80 memory
-struct Bus {
-    vdp: TMS9918A,
-    vdp_used: bool,
-    rom: [u8; 65536],
-}
-
-fn vec_to_array<T>(v: Vec<T>) -> [T; 65536] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", 65536, v.len()))
-}
-
-impl Io for Bus {
-    type Timestamp = i32;
-    type WrIoBreak = ();
-    type RetiBreak = ();
-
-    #[inline(always)]
-    fn write_io(&mut self, port: u16, data: u8, _ts: i32) -> (Option<()>, Option<NonZeroU16>) {
-        let masked_port = port & 0x00FF;
-        //println!("[write_io] masked_port: {:#X}, data: {:#X}", masked_port, data);
-        // VDP control
-        if masked_port == 0b00010010 {
-            //println!("[write_io] writing to VDP control port");
-            self.vdp_used = true;
-            self.vdp.write_control_port(data);
-        }
-
-        // VDP data
-        if masked_port == 0b00010000 {
-            //println!("[write_io] writing to VDP data port");
-            self.vdp_used = true;
-            self.vdp.write_data_port(data);
-        }
-
-        (None, None)
-    }
-
-    #[inline(always)]
-    fn read_io(&mut self, port: u16, _ts: i32) -> (u8, Option<NonZeroU16>) {
-        let masked_port = port & 0x00FF;
-        // VDP data
-        if masked_port == 0b00010000 {
-            //println!("[read_io ] reading from VDP data port");
-            self.vdp_used = true;
-            return (self.vdp.read_data_port(), None);
-        }
-
-        (0, None)
-    }
-}
-
-impl Memory for Bus {
-    type Timestamp = i32;
-    fn read_debug(&self, addr: u16) -> u8 {
-        self.rom[addr as usize]
-    }
-
-    fn write_mem(&mut self, addr: u16, value: u8, _ts: Self::Timestamp) {
-        self.rom[addr as usize] = value;
-    }
-}
-
-use error_chain::error_chain;
-use tempfile::Builder;
 
 error_chain! {
     foreign_links {
@@ -104,9 +32,7 @@ error_chain! {
     }
 }
 
-//extern crate image;
-
-use image::{ImageBuffer, Rgb, RgbImage, imageops};
+use image::{RgbImage, imageops};
 use libwebp::WebPDecodeRGB;
 
 struct Handler;
@@ -119,8 +45,7 @@ impl EventHandler for Handler {
 }
 
 #[group]
-//#[commands(about, say, boop, invert, shell, ping, join, leave, play, dm, z80)]
-#[commands(help, activity, say, boop, pfp, invert, ping, join, leave, play, stop, dm, z80, z80file)]
+#[commands(help, activity, say, boop, pfp, invert, ping, join, leave, play, stop, dm)]
 struct General;
 
 #[hook]
@@ -207,9 +132,7 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     `join`: join the current voice channel
     `leave`: leave the current voice channel
     `play`: play the specified URL, or search YouTube and play the first result
-    `stop`: stop any currently playing audio
-    `z80`: execute the specified opcodes (in hexadecimal) in the Z80 emulator and print the register results on halt
-    `z80file`: like `z80` except it loads the opcodes from the attached file";
+    `stop`: stop any currently playing audio";
     help_string.push_str(command_help_string);
 
     send_msg(&ctx, &msg, &help_string).await;
@@ -483,159 +406,6 @@ async fn invert(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
     send_file(&ctx, &msg, path).await;
 
     Ok(())
-}
-
-/*// execute a bash command and send the output
-#[command]
-async fn shell(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    let settings = if let Some(guild_id) = msg.guild_id {
-        ContentSafeOptions::default().clean_channel(false).display_as_member_from(guild_id)
-    } else {
-        ContentSafeOptions::default().clean_channel(false).clean_role(false)
-    };
-
-    let bash_args = content_safe(&ctx.cache, &args.rest(), &settings).await;
-    println!("{}", bash_args);
-    //let bash_args = args.rest();
-    let bash_shell = Command::new("bash").arg("-c").arg(bash_args).output()?;
-
-    let mut output_string = String::from("```");
-    let closing_string = String::from("```");
-    let bash_stdout = String::from_utf8_lossy(&bash_shell.stdout);
-    let bash_stderr = String::from_utf8_lossy(&bash_shell.stderr);
-    if bash_stdout != "" {
-        let bash_stdout_clean = bash_stdout.replace("```","`​`​`"); // add zero width spaces
-        output_string.push_str(&bash_stdout_clean);
-        output_string.push_str(&closing_string);
-    } else if bash_stderr != "" {
-        let bash_stderr_clean = bash_stdout.replace("```","`​`​`"); // add zero width spaces
-        output_string.push_str(&bash_stderr_clean);
-        output_string.push_str(&closing_string);
-    } else {
-        output_string = String::from("Command completed with no output on `stdout` or `stderr`");
-    }
-
-    send_msg(&ctx, &msg, &output_string).await;
-
-    Ok(())
-}*/
-
-// execute Z80 opcodes and print the resulting register contents
-#[command]
-async fn z80(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    let input = args.rest().split_whitespace().collect::<String>();
-    let memory_vec = hex::decode(input).unwrap();
-
-    z80_execute(&ctx, &msg, memory_vec).await
-}
-
-// execute Z80 opcodes from an uploaded file and print the resulting register contents
-#[command]
-async fn z80file(ctx: &Context, msg: &Message) -> CommandResult {
-    let memory_vec = msg.attachments[0].download().await.unwrap();
-
-    z80_execute(&ctx, &msg, memory_vec).await
-}
-
-async fn z80_execute(ctx: &Context, msg: &Message, mut memory_vec: Vec<u8>) -> CommandResult {
-    let mut tsc = TsClock::default();
-    let mut cpu = Z80CMOS::default();
-
-    // fill the remaining memory with halt opcodes
-    for _ in 0..65536-memory_vec.len() {
-        memory_vec.push(0x76);
-    }
-
-    let mut memory = Bus { vdp: TMS9918A::new(), vdp_used: false, rom: vec_to_array(memory_vec) };
-
-    let mut disassembly_string = String::from("Disassembly:\n```");
-
-    let mut flag = false;
-    let _ = disasm::disasm_memory::<Z80CMOS, _, ()>(0x0000, &mut memory.rom, 
-        |debug| {
-            disassembly_string.push_str(&format!("{}\n", format_args!("{:#X}", debug)));
-            // if two halt instructions are found in a row, exit
-            if debug.code.as_slice() == [0x76] {
-                if flag == true {
-                    return Err(());
-                }
-                flag = true;
-            } else {
-                flag = false;
-            }
-            Ok(())
-        });
-
-    disassembly_string.push_str("```");
-    send_msg(&ctx, &msg, &disassembly_string).await;
-    println!("{}", &disassembly_string);
-
-    cpu.reset();
-    loop {
-        match cpu.execute_next(&mut memory, &mut tsc,
-                Some(|_| print!("") )) {
-            Err(BreakCause::Halt) => { break }
-            _ => {}
-        }
-    }
-
-    memory.vdp.update();
-
-    let reg_a = cpu.get_reg(Reg8::A, None);
-    let reg_bc = cpu.get_reg16(StkReg16::BC);
-    let reg_de = cpu.get_reg16(StkReg16::DE);
-    let reg_hl = cpu.get_reg16(StkReg16::HL);
-
-    let mut reg_string = String::from("Z80 register contents on halt:\n");
-
-    reg_string.push_str(&format!("`A:  {:#04X}`\n", reg_a));
-    reg_string.push_str(&format!("`BC: {:#06X}`\n", reg_bc));
-    reg_string.push_str(&format!("`DE: {:#06X}`\n", reg_de));
-    reg_string.push_str(&format!("`HL: {:#06X}`\n", reg_hl));
-
-    send_msg(&ctx, &msg, &reg_string).await;
-
-    reg_string = String::from("TMS9918A register contents on halt:\n");
-
-    reg_string.push_str(&format!("`0: {:#04X} | 4: {:#04X}`\n", memory.vdp.read_register(0), memory.vdp.read_register(4)));
-    reg_string.push_str(&format!("`1: {:#04X} | 5: {:#04X}`\n", memory.vdp.read_register(1), memory.vdp.read_register(5)));
-    reg_string.push_str(&format!("`2: {:#04X} | 6: {:#04X}`\n", memory.vdp.read_register(2), memory.vdp.read_register(6)));
-    reg_string.push_str(&format!("`3: {:#04X} | 7: {:#04X}`\n", memory.vdp.read_register(3), memory.vdp.read_register(7)));
-
-    if memory.vdp_used == false {
-        Ok(())
-    } else {
-        send_msg(&ctx, &msg, &reg_string).await;
-
-        let vdp_file = Builder::new().suffix(".png").tempfile()?;
-
-        let width = memory.vdp.frame_width as u32;
-        let height = memory.vdp.frame_height as u32;
-        let mut pixel_buf = ImageBuffer::from_fn(width, height, |x, y| {
-            Rgb([
-                ((memory.vdp.frame[((y * width) + x) as usize] & 0xFF0000) >> 16) as u8,
-                ((memory.vdp.frame[((y * width) + x) as usize] & 0x00FF00) >> 8) as u8,
-                (memory.vdp.frame[((y * width) + x) as usize] & 0x0000FF) as u8,
-            ])
-        });
-
-        pixel_buf = imageops::resize(&mut pixel_buf, width * 4, height * 4, imageops::FilterType::Nearest);
-
-        let file_path = match vdp_file.path().to_str() {
-            Some(file_path) => file_path,
-            None => {
-                send_msg(&ctx, &msg, "Failed to get file path of image buffer for emulated TMS9918A").await;
-                return Ok(())
-            }
-        };
-        send_msg(&ctx, &msg, "TMS9918A video output:").await;
-        println!("temp file location: {:?}", file_path);
-        pixel_buf.save(file_path).unwrap();
-        let path = vec![file_path];
-        send_file(&ctx, &msg, path).await;
-
-        Ok(())
-    }
 }
 
 #[command]
